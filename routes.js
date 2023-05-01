0dfd149441ceb8906d1989cac3403ac5be86df69
Richard Virag <andraderv10@gmail.com>
2023-05-01 12:48:30 -0300

    Portfolio and adjustment

--- routes.js
+++ routes.js
@@ -1,26 +1,31 @@
 module.exports = (server) => {
   server.get("/", (req, res) => {
-    server.locals.title = 'Design além da solução';
+    server.locals.title = 'Design muito além da solução';
     res.render('index', { whiteHeader: true });
   });
 
   server.get("/contato", (req, res) => {
-    server.locals.title = 'Contate-nos';
+    server.locals.title = 'Contate-nos agora mesmo';
     res.render('contact', { whiteHeader: true });
   });
 
   server.get("/servicos", (req, res) => {
-    server.locals.title = 'O que fazemos de melhor';
+    server.locals.title = 'A melhor experiência para você';
     res.render('services');
   });
 
   server.get("/sobre", (req, res) => {
-    server.locals.title = 'Descubra sobre nós';
+    server.locals.title = 'Descubra mais sobre nós';
     res.render('about', { whiteHeader: true });
   });
 
+  server.get("/portfolio", (req, res) => {
+    server.locals.title = 'O que fazemos de melhor em nossos clientes';
+    res.render('portfolio');
+  });
+
   server.use((req, res) => {
-    server.locals.title = '404';
+    server.locals.title = 'Ops, algo de errado não está certo';
     res.status(404).render('404', { whiteHeader: true });
   })
 };
